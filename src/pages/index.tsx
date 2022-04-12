63e5aef759216b49e076249ea9ba964cafc6df08
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-04-12 09:19:13 +0900

    不要なconsole.logの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,10 @@ import "react-tabs/style/react-tabs.css";
 import Link from "next/link";
 import Props from "../components/types";
 import Image from "next/image";
-import { parseISO, format } from "date-fns";
+import { format } from "date-fns";
 import ja from "date-fns/locale/ja";
 import Advertisement from "../components/Advertisement";
 import SideBlank from "../components/SideBlank";
-// import styles from "../styles/globals.css";
 
 // 今日の日付
 const Today = format(
@@ -22,10 +21,6 @@ const Today = format(
 );
 
 const Home = (props: any) => {
-  console.log("NJ" + JSON.stringify(props.topicArticles));
-  // console.log("W" + props.weatherNews);
-  // console.log("WJ" + JSON.stringify(props.weatherNews));
-
   return (
     <>
       <title>Wahoo! NIPPON</title>
@@ -113,31 +108,26 @@ export const getServerSideProps = async () => {
   const topicRes = await fetch(urlTopic);
   const topicJson = await topicRes.json();
   const topicArticles = await topicJson?.articles;
-  // const topicArticles = JSON.stringify(topicJson?.articles);
   // ビジネス用
   const urlBusiness = `https://newsapi.org/v2/top-headlines?country=jp${pageSizeForTop}category=business&country=jp&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`;
   const businessRes = await fetch(urlBusiness);
   const businessJson = await businessRes.json();
   const businessArticles = await businessJson?.articles;
-  // const businessArticles = JSON.stringify(businessJson?.articles);
   // 科学技術
   const urlScience = `https://newsapi.org/v2/top-headlines?country=jp${pageSizeForTop}category=science&country=jp&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`;
   const scienceRes = await fetch(urlScience);
   const scienceJson = await scienceRes.json();
   const scienceArticles = await scienceJson?.articles;
-  // const scienceArticles = JSON.stringify(scienceJson?.articles);
   // エンタメ
   const urlEntertainment = `https://newsapi.org/v2/top-headlines?country=jp${pageSizeForTop}category=entertainment&country=jp&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`;
   const entertainmentRes = await fetch(urlEntertainment);
   const entertainmentJson = await entertainmentRes.json();
   const entertainmentArticles = await entertainmentJson?.articles;
-  // const entertainmentArticles = JSON.stringify(entertainmentJson?.articles);
   // スポーツ
   const urlSports = `https://newsapi.org/v2/top-headlines?country=jp${pageSizeForTop}category=sports&country=jp&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`;
   const sportsRes = await fetch(urlSports);
   const sportsJson = await sportsRes.json();
   const sportsArticles = await sportsJson?.articles;
-  // const sportsArticles = JSON.stringify(sportsJson?.articles);
 
   // OpenWeatherMapから天気情報を取得
   const lat = 35.690921;
