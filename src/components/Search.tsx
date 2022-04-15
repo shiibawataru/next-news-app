b85c63025427951074ea83ac3e5e45d6361aa5da
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-04-15 14:40:10 +0900

    検索エンジンの追加

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,19 +4,26 @@ import styles from "./Search.module.css";
 
 const Search = () => {
   return (
-    <div className={`${styles.body} w-100`}>
-      <div className="d-flex align-items-center input-group w-50">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="まだ何も検索できません"
-        />
-        <button className="btn btn-primary" type="button">
-          <BiSearchAlt />
-          検索
-        </button>
+    <>
+      <div className={`${styles.body} w-100`}>
+        {/* <div className="d-flex align-items-center input-group w-50">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="まだ何も検索できません"
+          />
+          <button className="btn btn-primary" type="button">
+            <BiSearchAlt />
+            検索
+          </button> */}
+        <script
+          async
+          src={`https://cse.google.com/cse.js?cx=${process.env.NEXT_PUBLIC_GOOGLESEARCHENGINE_ID}`}
+        ></script>
+        <div className="gcse-search"></div>
       </div>
-    </div>
+      {/* </div> */}
+    </>
   );
 };
 
